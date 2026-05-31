7dc12e8c18bc92186d73872c3da17bdcfb9cbf43
mike-b-m <mikebarrymichel@gmail.com>
2026-05-31 13:11:43 -0400

    update

--- app/components/Result/Result.tsx
+++ app/components/Result/Result.tsx
@@ -72,11 +72,13 @@ function Result() {
                     <div className="text-gray-900 font-bold "><span className="ml">{user.nature}</span></div>
                     <div className="text-gray-900 mb-4">{user.def}</div>
                     <h1 className="bg-clo h-1 mt-2 mb-2">_</h1>
-                    <div><span className="font-bold">Sinonim:</span> {user.sino.map((sino:any) => (
-                      <a href={`/results?search=${sino}`} className="hover:text-blue-500 cursor-pointer">{sino}, </a>
+                    <div><span className="font-bold">Sinonim:</span> {user.sino.map((sino:string) => (
+                      <a href={`/results?search=${sino}`}key={sino} className="hover:text-blue-500 cursor-pointer">{sino}, </a>
                     ))}</div>
                     <h1 className="bg-clo h-1 mt-2 mb-2">_</h1>
-                    <div><span className="font-bold">Kont: </span>{user.kont}</div>
+                    <div><span className="font-bold">Kont: </span>{user.kont.map((kont:string) => (
+                      <a href={`/results?search=${kont}`} key={kont} className="hover:text-blue-500 cursor-pointer">{kont}, </a>
+                    ))}</div>
                   </li>
                 ))}
               </ul>
